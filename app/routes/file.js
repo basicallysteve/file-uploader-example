a4368543e3586aa60513b9a644e965a0b364c8a2
Steven Turner <steven.turner@rothco.com>
2021-08-31 22:27:51 -0400

    Integrated Vue Frontend

--- app/routes/file.js
+++ app/routes/file.js
@@ -6,9 +6,11 @@ let PayloadResponse = require("../helpers/Response");
 router.post('/upload', function(req, res) {
     let file;
     
-  
+    console.log(req);
     if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).send('No files were uploaded.');
+      let errorPayload = new PayloadResponse();
+      errorPayload.append('message', 'No files were uploaded.')
+      return res.status(400).send(errorPayload.respond());
     }
   
     // The name of the input field (i.e. "file") is used to retrieve the uploaded file
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const app = require("express")();
+const cors = require("cors");
 const fileUpload = require('express-fileupload');
 const fileRoutes = require('./app/routes/file');
+app.use(cors())
 
 app.use(fileUpload());
 
